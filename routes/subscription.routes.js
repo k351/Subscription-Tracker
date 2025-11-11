2865a7995dee82d50cc5790c13ea74e928ed8959
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2025-11-11 21:52:00 +0700

    Setup router for auth, users, and subscriptions

---
+++ routes/subscription.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req, res) => {
+    res.send({ title: 'GET all subscriptions' });
+});
+
+subscriptionRouter.get('/:id', (req, res) => {
+    res.send({ title: 'GET subscription details' });
+});
+
+subscriptionRouter.post('/', (req, res) => {
+    res.send({ title: 'CREATE subscription' });
+});
+
+subscriptionRouter.put('/:id', (req, res) => {
+    res.send({ title: 'UPDATE a subscription' });
+});
+
+subscriptionRouter.delete('/:id', (req, res) => {
+    res.send({ title: 'DELETE a subscription' });
+});
+
+// Additional subscription-specific routes
+
+subscriptionRouter.get('/users/:id', (req, res) => {
+    res.send({ title: 'GET all user subscriptions' });
+});
+
+subscriptionRouter.put('/:id/cancel', (req, res) => {
+    res.send({ title: 'CANCEL subscription' });
+});
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {
+    res.send({ title: 'GET upcoming subscription' });
+});
+
+export default subscriptionRouter;
